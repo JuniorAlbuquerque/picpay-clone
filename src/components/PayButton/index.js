bfebfe0ab990ddcc712f033ac3628c779531d68d
Júnior Albuquerque <junioralbuquerque81@gmail.com>
2020-07-16 14:24:41 -0400

    style: PayButton component

--- src/components/PayButton/index.js
+++ src/components/PayButton/index.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+import { TouchableWithoutFeedback } from 'react-native';
+
+import { Button, Label } from './styles';
+
+export default function PayButton({ onPress, focused }) {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <Button colors={focused ? ['#fff', '#ccc'] : ['#00fc5c', '#00ac4a']}>
+        <MaterialIcons
+          name="attach-money"
+          size={30}
+          color={focused ? '#000' : '#fff'}
+        />
+        <Label focused={focused}>Pagar</Label>
+      </Button>
+    </TouchableWithoutFeedback>
+  );
+}
